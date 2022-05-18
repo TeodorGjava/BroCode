9d4514253397c200563140ae56d967dc833a3f4a
Teodor Georgiev <tgeorgievmk2@gmail.com>
2022-05-18 12:24:59 +0300

    Abstract

---
+++ src/Abstract/Car.java
@@ -0,0 +1,9 @@
+package Abstract;
+//subclass of Vehicle
+public class Car extends Vehicle{
+    @Override
+    void go(){
+        System.out.println("The driver drives the car");
+    }
+
+}
---
+++ src/Abstract/Main.java
@@ -0,0 +1,11 @@
+package Abstract;
+
+public class Main {
+    public static void main(String[] args) {
+        Car car = new Car();
+        //cannot declare a vehicle obj, we have to pick a certain type of vehicle, a car, a motorcycle, a truck
+        Vehicle vehicle = new Car();
+        car.go();
+        vehicle.go();
+    }
+}
